9977d8767260caa0701f24374488698cc4f5116d
Sander Pool <spool@145-24-243-55.wlan.hro.nl>
2017-03-28 14:24:56 +0200

    added specific project options

--- app/project.component.ts
+++ app/project.component.ts
@@ -2,16 +2,60 @@
 // typescript definitie voor fetch. VS Code herkent d.ts files automatisch, webstorm niet getest
 
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PostService } from './services/posts.service';
 
 @Component({
   selector: 'project-list',
   templateUrl: './app/project.component.html',
-  styleUrls: ['./app/project.component.css']
+  styleUrls: ['./app/project.component.css'],
+  providers: [PostService]
 })
-export class ProjectComponent {
-    constructor() {
+export class ProjectComponent implements OnInit {
+
+    title: string = "Project Details";
+    projectId: string;
+    private sub: any;
+
+    name: string;
+    developer: string;
+    client: string;
+    completed: boolean;
+    completedText: string;
+
+    constructor(private _postService: PostService, private _route: ActivatedRoute, private _router: Router) {
         
     }
+
+    ngOnInit() {
+      this.sub = this._route.params.subscribe(params => {
+        this.projectId = params['id'];
+      });
+    }
+
+    getProjectInformation() {
+        this._postService.getSpecificProject(this.projectId).subscribe(proj => {   
+        this.name = proj.name;
+        this.developer = proj.developer;
+        this.client = proj.client;
+        this.completed = proj.completed;
+
+
+         if(!this.completed){ 
+             this.completedText = 'Unfinished';
+         } else {
+             this.completedText = 'Finished';
+           }
+        });
+    }
+
+    removeProject() {
+        this._postService.deleteProject(this.projectId).subscribe();
+        this._router.navigate(['/']); // Redirect
+    }
+
+    editProject() {
+        this._router.navigate(['/edit/', this.projectId]); // Redirect
 
 }
--- app/services/posts.service.ts
+++ app/services/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers} from '@angular/http';
+import { Http, Headers, RequestOptions} from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
@@ -17,11 +17,26 @@ export class PostService {
         .map(res => res.json());
     }
 
+    getSpecificProject(id: string) {
+        return this.http.get(apiLink + id)
+            .map(res => res.json());
+    }
+
     addProject(newProject: any) {
         var headers = new Headers();
         headers.append('content-type', 'application/json');
         
         return this.http.post(apiLink, JSON.stringify(newProject), {headers:headers})
         .map(res => res.json());
+    }
+
+    editProject(project: any, id: string) {
+        var headers = new Headers();
+        headers.append('content-type', 'application/json');
+        let options = new RequestOptions({headers: headers });
+    }
+
+    deleteProject(id : string) {
+        return this.http.delete(apiLink + id);
     }
 }
