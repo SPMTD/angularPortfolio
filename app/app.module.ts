69302b4f1216da8f3ece2d6faba891f8715d9115
Sander Pool <spool@145-24-243-55.wlan.hro.nl>
2017-03-28 13:04:15 +0200

    Able to upload

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+// import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 
@@ -11,7 +11,7 @@ import { ProjectsComponent } from './projects.component';
 import { ProjectComponent } from './project.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpModule, Observable],
+  imports:      [ BrowserModule, FormsModule, HttpModule],
   declarations: [ AppComponent, ProjectsComponent, ProjectComponent ],
   bootstrap:    [ AppComponent ]
 })
